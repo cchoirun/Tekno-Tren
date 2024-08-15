846ccb1d08ca66d21df175c75403733e6c55e98a
cchoirun <niamchoirun020#gmail.com>
2024-08-15 08:43:14 +0700

    fix: update Chunk Size Warning Limit to 1000 kb

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Ukuran default adalah 500KB, sesuaikan sesuai kebutuhan Anda.
+  },
 })
